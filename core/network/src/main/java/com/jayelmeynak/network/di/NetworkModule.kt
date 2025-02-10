01430d8003ff65296dedf9cbeac70e164d2cbe75
jayelmeynak <magomed110301@yandex.ru>
2025-02-11 01:02:06 +0300

    add di for network

---
+++ core/network/src/main/java/com/jayelmeynak/network/di/NetworkModule.kt
@@ -0,0 +1,45 @@
+package com.jayelmeynak.network.di
+
+import com.jayelmeynak.network.data.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://api.deezer.com"
+    private const val TIMEOUT = 20L
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun provideApi(okHttpClient: OkHttpClient): ApiService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
