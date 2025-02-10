cb623efd76f364695051cab8fe7a00ada6f22ab1
jayelmeynak <magomed110301@yandex.ru>
2025-02-11 02:55:31 +0300

    add dependencies in module

--- features/search-tracks/build.gradle.kts
+++ features/search-tracks/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -37,10 +39,33 @@ dependencies {
     implementation(project(":core:ui"))
     implementation(project(":core:network"))
 
+    implementation (libs.androidx.lifecycle)
+    implementation (libs.androidx.lifecycle.runtime.compose)
+    implementation (libs.androidx.hilt.navigation.compose)
+    implementation (libs.accompanist.flowlayout)
+    implementation (libs.coil.compose)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation (libs.androidx.paging.compose)
+    implementation(libs.androidx.ui.util)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.android)
+    implementation (libs.androidx.hilt.navigation.compose)
+    implementation (libs.androidx.hilt.navigation.compose.v100alpha01)
 }
