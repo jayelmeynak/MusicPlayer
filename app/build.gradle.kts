5a0d6792c1b12236139a333d2ef82e2fe7aabfa7
Magomed Mazhitov <m.mazhitov@vkteam.ru>
2026-04-22 00:52:38 +0300

    refactor: delete unused compileOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,10 +28,6 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
     kotlin {
         jvmToolchain(17)
     }
--- core/local/build.gradle.kts
+++ core/local/build.gradle.kts
@@ -24,10 +24,6 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
     kotlin {
         jvmToolchain(17)
     }
